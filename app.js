8787e61d0bbbcbf8c326c975c7fa2e3c476c9bee
Basel Askar <baselaskar91@gmail.com>
2022-10-27 15:49:29 +0200

    optimzing some colors

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ card.addEventListener('mousemove',(e) => {
 
   const simiWidth = card.offsetWidth / 2;
   const simiHeight = card.offsetHeight / 2;
-  const rotY = ((px - simiWidth) / simiWidth) * 10;
-  const rotX = -((py - simiHeight) / simiHeight) * 10;
+  const rotY = ((px - simiWidth) / simiWidth) * 20;
+  const rotX = -((py - simiHeight) / simiHeight) * 20;
 
 
   const shX = rotX * 3;
@@ -21,12 +21,12 @@ card.addEventListener('mousemove',(e) => {
 
 
   card.style.transform = `perspective(50rem) rotateX(${rotX}deg) rotateY(${rotY}deg)`;
-  card.style['box-shadow'] = `${-shY}px ${shX}px 20px rgba(230, 97, 97, 0.548)`;
+  card.style['box-shadow'] = `${-shY}px ${shX}px 20px rgba(0, 0, 0, 0.4)`;
 });
 
 card.addEventListener('mouseleave',(e) => {
   card.style.transform = '';
-  card.style['box-shadow'] = '0px 0px 20px rgba(230, 97, 97, 0.548)'
+  card.style['box-shadow'] = '0px 0px 20px rgba(0, 0, 0, 0.4)'
 });
 
 
